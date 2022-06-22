c0c61c4be36904a690f907e640b8dade56426616
link199292 <ludovico.valenti@gmail.com>
2022-06-22 19:02:50 +0200

    fixed error causing error while sending requests

--- main.py
+++ main.py
@@ -3,57 +3,63 @@ import json
 import grequests
 from index_generator import Cities, create_request
 
+N_BATCH = 100
+
 
 def exception_handler(request, exception):
     print(f"Request failed")
 
 
-def send_requests():
+def send_requests(batch):
     processed = 0
     rs = (grequests.get(i, timeout=10) for i in batch)
     res = grequests.map(rs, exception_handler=exception_handler)
     text = list(map(lambda d: d.text if d else None, res))
-    for i, j in zip(res, text):
-        if i.status_code == 200:
-            processed += 1
+
     to_ret = []
-    for i in text:
-        if i is None:
-            r.lpush('unsent_requests', i)
-        else:
-            to_ret.append(i)
+    with r.pipeline() as pipe:
+        for i, j in zip(batch, text):
+            if i is None or eval(j)['status'] != 'ok':
+                pipe.lpush('unsent_requests', i)
+            else:
+                processed += 1
+                to_ret.append(j)
+        pipe.execute()
 
     print(f"{round((processed / len(batch) * 100), 2)}% correctly processed")
     return to_ret
+
+
+def batch_executor(n_batch=N_BATCH):
+    batch_counter = 0
+    with r.pipeline() as pipe:
+        while batch_counter <= n_batch:
+            pipe.rpop('unsent_requests')
+            batch_counter += 1
+        extracted_batch = pipe.execute()
+    return send_requests(extracted_batch)
 
 
 with open('european_countries.json') as read_file1, open('secrets.json') as read_file2:
     list_of_cities = json.load(read_file1)
     token = json.load(read_file2)['token']
 
-batch_counter = 0
-batch = []
-
 if __name__ == "__main__":
-    r = redis.Redis(host='localhost', port=6379, db=0)
+    r = redis.RedisCluster(host='localhost', port=30001, decode_responses=True)
     r.flushdb()
     all_cities = Cities(list_of_cities)
-    for city in all_cities:
-        curr_req = create_request(city, token)
-        r.lpush('unsent_requests', curr_req)
 
-    while r.llen('unsent_requests') != 0:
-        if batch_counter >= 100:
-            print('reached 100 requests')
-            batch_counter = 0
-            for link in batch:
-                x = link.decode('ASCII')
-                a, b = x.split('geo')
-                x = f"{a}geo{b}"
-            result = send_requests()
-            # reinsert into Redis and sort (use Redis ordered list)
-            batch = []
+    with r.pipeline() as pipe:
+        for city in all_cities:
+            curr_req = create_request(city, token)
+            pipe.lpush('unsent_requests', curr_req)
+        pipe.execute()
+
+    len_monitor = r.llen('unsent_requests')
+    while r.llen('unsent_requests') > 0:
+        curr_len = r.llen('unsent_requests')
+        if len_monitor == curr_len and curr_len < N_BATCH:
+            batch_executor(n_batch=curr_len)
         else:
-            x = r.rpop('unsent_requests')
-            batch.append(x)
-            batch_counter += 1
+            batch_executor(n_batch=N_BATCH)
+        len_monitor = curr_len
